a3a17c524e4ab52e660f142bc61c9b4a353e1b19
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-02-12 13:03:53 -0800

    Add setup.py and setup.cfg.

---
+++ setup.py
@@ -0,0 +1,52 @@
+"""
+    ydf
+    ~~~
+
+    YAML to Dockerfile.
+
+    :copyright: (c) 2017 Andrew Hawker.
+    :license: Apache 2.0, see LICENSE file.
+"""
+
+import ast
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+version_regex = re.compile(r'__version__\s+=\s+(.*)')
+
+
+def get_version():
+    with open('ydf/__init__.py', 'r') as f:
+        return str(ast.literal_eval(version_regex.search(f.read()).group(1)))
+
+
+setup(
+    name='ydf',
+    version=get_version(),
+    author='Andrew Hawker',
+    author_email='andrew.r.hawker@gmail.com',
+    url='https://github.com/ahawker/ydf',
+    license='Apache 2.0',
+    description='YAML to Dockerfile',
+    long_description=__doc__,
+    packages=['ydf'],
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    )
+)
