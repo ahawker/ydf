c9f52bb9a8deb8595584110f53833f38c324e13a
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-02-12 12:53:04 -0800

    Add formatting module.

---
+++ tests/test_formatting.py
@@ -0,0 +1,15 @@
+"""
+    test_formatting
+    ~~~~~~~~~~~~~~~
+
+    Tests for the :mod:`~ydf.formatting` module.
+"""
+
+import pytest
+
+from ydf import formatting
+
+
+@pytest.mark.xfail()
+def test_stub():
+    assert False, "TODO: Implement"
---
+++ ydf/formatting.py
@@ -0,0 +1,36 @@
+"""
+    ydf/formatting
+    ~~~~~~~~~~~~~~
+
+    Helper functions for cleanly formatting instructions.
+"""
+
+
+def list_with_conditional_line_breaks(lst, line_break=' \\\n', indent=4, quote_escape=False):
+    """
+    Build a string with line breaks & indentation for lists with more than one item.
+
+    :param lst: Collection of items to line separate
+    :param line_break: String used to separate each item
+    :param indent: Number of spaces used to indent each new line
+    :param quote_escape: Optional flag to indicate if each item should be escaped with double quotes
+    :return: Multi-line string that is well formed for human readers
+    """
+    value_fmt = '"{}"' if quote_escape else '{}'
+    return (line_break + ' ' * indent).join(value_fmt.format(v) for v in lst)
+
+
+def dict_with_conditional_line_breaks(dct, delimiter='=', line_break=' \\\n', indent=4, quote_escape=False):
+    """
+    Build a string with line breaks & indentation for dicts with more than one item.
+
+    :param dct: Collection of key/values to line separate
+    :param delimiter: Delimiter character that should be displayed between each key/value pair
+    :param line_break: String used to separate each item
+    :param indent: Number of spaces used to indent each new line
+    :param quote_escape: Optional flag to indicate if each key and value should be escaped with double quotes
+    :return: Multi-line string that is well formed for human readers
+    """
+    key_value_fmt = '"{}"' if quote_escape else '{}'
+    return (line_break + ' ' * indent).join(key_value_fmt.format(k) + delimiter + key_value_fmt.format(v)
+                                            for k, v in dct.items())
