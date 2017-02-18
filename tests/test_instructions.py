e2a98a9f47100263ffe0727939acb9b4c3bf1e6a
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-02-18 09:53:31 -0800

    Add instructions module.

---
+++ tests/test_instructions.py
@@ -0,0 +1,15 @@
+"""
+    test_instructions
+    ~~~~~~~~~~~~~~~~~
+
+    Tests for the :mod:`~ydf.instructions` module.
+"""
+
+import pytest
+
+from ydf import instructions
+
+
+@pytest.mark.xfail()
+def test_stub():
+    assert False, "TODO: Implement"
---
+++ ydf/instructions.py
@@ -0,0 +1,27 @@
+"""
+    ydf/instructions
+    ~~~~~~~~~~~~~~~~
+
+    Convert objects parsed from YAML to those that represent Dockerfile instructions.
+"""
+
+__all__ = []
+
+
+FROM = 'FROM'
+RUN = 'RUN'
+CMD = 'CMD'
+LABEL = 'LABEL'
+EXPOSE = 'EXPOSE'
+ENV = 'ENV'
+ADD = 'ADD'
+COPY = 'COPY'
+ENTRYPOINT = 'ENTRYPOINT'
+VOLUME = 'VOLUME'
+USER = 'USER'
+WORKDIR = 'WORKDIR'
+ARG = 'ARG'
+ONBUILD = 'ONBUILD'
+STOPSIGNAL = 'STOPSIGNAL'
+HEALTHCHECK = 'HEALTHCHECK'
+SHELL = 'SHELL'
