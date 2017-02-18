65bfede8d8739699e57ddd4f66049ac0374d1a8d
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-02-18 09:59:55 -0800

    Add @instruction decorator to mark module level funcs as handlers.

--- ydf/instructions.py
+++ ydf/instructions.py
@@ -5,6 +5,12 @@
     Convert objects parsed from YAML to those that represent Dockerfile instructions.
 """
 
+import collections
+import functools
+
+from ydf import meta
+
+
 __all__ = []
 
 
@@ -25,3 +31,33 @@ ONBUILD = 'ONBUILD'
 STOPSIGNAL = 'STOPSIGNAL'
 HEALTHCHECK = 'HEALTHCHECK'
 SHELL = 'SHELL'
+
+
+def get_instructions():
+    """
+    Get all functions within this module that are decorated with :func:`~ydf.instructions.instruction`.
+    """
+    instructions = collections.defaultdict(dict)
+    for func in (value for key, value in globals().items() if meta.is_instruction(value)):
+        instructions[func.instruction_name][func.instruction_type] = func
+    return instructions
+
+
+def instruction(name, type, desc):
+    """
+    Decorate a function to indicate that it is responsible for converting a python type to a Docker
+    instruction.
+
+    :param name: Name of docker instruction
+    :param type: Type of python object it can convert
+    :param desc: Short description of expected format for the python object.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return '{} {}'.format(name, func(*args, **kwargs))
+        wrapper.instruction_name = name
+        wrapper.instruction_type = type
+        wrapper.instruction_desc = desc
+        return wrapper
+    return decorator
