1a8b35c5d6e522249eaea5d08e2bc6c1dd0e6ab4
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-02-18 11:28:38 -0800

    Break out duplicate formatting logic into small helper functions.

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -10,6 +10,91 @@ import pytest
 from ydf import formatting
 
 
-@pytest.mark.xfail()
-def test_stub():
-    assert False, "TODO: Implement"
+@pytest.fixture(scope='module', params=[
+    ['foo'],
+    ['bar'],
+    ['baz']
+])
+def one_item_list(request):
+    """
+    Fixture that yields one item lists.
+    """
+    return request.param
+
+
+@pytest.fixture(scope='module', params=[
+    ['foo', 'bar'],
+    ['foo', 'bar', 'baz'],
+    ['foo', 'bar', 'baz', 'jaz'],
+    ['foo', 'bar', 'baz', 'jaz', 'faz'],
+    ['foo', 'bar', 'baz', 'jaz', 'faz', 'zaz']
+])
+def multi_item_list(request):
+    """
+    Fixture that yields multi item lists.
+    """
+    return request.param
+
+
+@pytest.fixture(scope='module', params=[
+    dict(name='foo'),
+    dict(foo='bar'),
+    dict(bar='baz')
+])
+def one_item_dict(request):
+    """
+    Fixture that yields one item dicts.
+    """
+    return request.param
+
+
+@pytest.fixture(scope='module', params=[
+    dict(foo='foo'),
+    dict(foo='foo', bar='bar'),
+    dict(foo='foo', bar='bar', baz='baz'),
+    dict(foo='foo', bar='bar', baz='baz', jaz='jaz'),
+    dict(foo='foo', bar='bar', baz='baz', jaz='jaz', faz='faz'),
+    dict(foo='foo', bar='bar', baz='baz', jaz='jaz', faz='faz', zaz='zaz')
+])
+def multi_item_dict(request):
+    """
+    Fixture that yields multi item dicts.
+    """
+    return request.param
+
+
+def test_list_doesnt_line_break_on_single_item(one_item_list):
+    """
+    Assert that :func:`~ydf.formatting.list_with_conditional_line_breaks` doesn't insert a line break
+    for lists with only one item.
+    """
+    assert formatting.list_with_conditional_line_breaks(one_item_list) == one_item_list[0]
+
+
+def test_list_uses_given_line_break(multi_item_list):
+    """
+    Assert that :func:`~ydf.formatting.list_with_conditional_line_breaks` uses the given line break
+    string as expected.
+    """
+    line_break = '$'
+    expected = '{}{}'.format(line_break, ' ' * formatting.DEFAULT_INDENT).join(multi_item_list)
+    assert formatting.list_with_conditional_line_breaks(multi_item_list, line_break) == expected
+
+
+def test_list_uses_given_indentation(multi_item_list):
+    """
+    Assert that :func:`~ydf.formatting.list_with_conditional_line_breaks` uses the given indentation size
+    as expected.
+    """
+    indent = 2
+    expected = '{}{}'.format(formatting.DEFAULT_LINE_BREAK, ' ' * indent).join(multi_item_list)
+    assert formatting.list_with_conditional_line_breaks(multi_item_list, indent=indent) == expected
+
+
+def test_dict_doesnt_line_break_on_single_item(one_item_dict):
+    """
+    Assert that :func:`~ydf.formatting.dict_with_conditional_line_breaks` doesn't insert a line break
+    for dicts with only one item.
+    """
+    kvp = list(one_item_dict.items())[0]
+    assert formatting.dict_with_conditional_line_breaks(one_item_dict) == '{}={}'.format(*kvp)
--- ydf/formatting.py
+++ ydf/formatting.py
@@ -6,7 +6,51 @@
 """
 
 
-def list_with_conditional_line_breaks(lst, line_break=' \\\n', indent=4, quote_escape=False):
+DEFAULT_LINE_BREAK = ' \\\n'
+DEFAULT_INDENT = 4
+DEFAULT_QUOTE_ESCAPE = False
+DEFAULT_KEY_VALUE_DELIMITER = '='
+DEFAULT_STR_JOIN_DELIMITER = None
+
+
+def _escape(value, escape=DEFAULT_QUOTE_ESCAPE):
+    """
+    Optionally double-quote escape the given string value.
+
+    :param escape: Flag to indicate if the given string should be quote escaped.
+    :return: String that is optionally escaped.
+    """
+    template = '"{}"' if escape else '{}'
+    return template.format(value)
+
+
+def _break(line_break=DEFAULT_LINE_BREAK, indent=DEFAULT_INDENT):
+    """
+    Build a string that represents a line break with an indentation to align with
+    the previous line.
+
+    :param line_break: String to use for a line break
+    :param indent: Number of spaces to indent after the line break
+    :return: String that performs a line break and indentation to align
+    """
+    return line_break + ' ' * indent
+
+
+def _line_break_join(lst, line_break=DEFAULT_LINE_BREAK, indent=DEFAULT_INDENT):
+    """
+    Build a multi-line string using the given line break and alignment indentation size for
+    each item in the list.
+
+    :param lst: List of items to separate with indented line breaks
+    :param line_break: String to use for each line break
+    :param indent: Number of spaces for alignment indentation
+    :return: Multi-line string with indented line breaks for each item
+    """
+    return _break(line_break, indent).join(lst)
+
+
+def list_with_conditional_line_breaks(lst, line_break=DEFAULT_LINE_BREAK, indent=DEFAULT_INDENT,
+                                      quote_escape=DEFAULT_QUOTE_ESCAPE):
     """
     Build a string with line breaks & indentation for lists with more than one item.
 
@@ -16,11 +60,11 @@ def list_with_conditional_line_breaks(lst, line_break=' \\\n', indent=4, quote_e
     :param quote_escape: Optional flag to indicate if each item should be escaped with double quotes
     :return: Multi-line string that is well formed for human readers
     """
-    value_fmt = '"{}"' if quote_escape else '{}'
-    return (line_break + ' ' * indent).join(value_fmt.format(v) for v in lst)
+    return _line_break_join((_escape(v, quote_escape) for v in lst), line_break, indent)
 
 
-def dict_with_conditional_line_breaks(dct, delimiter='=', line_break=' \\\n', indent=4, quote_escape=False):
+def dict_with_conditional_line_breaks(dct, delimiter=DEFAULT_KEY_VALUE_DELIMITER, line_break=DEFAULT_LINE_BREAK,
+                                      indent=DEFAULT_INDENT, quote_escape=DEFAULT_QUOTE_ESCAPE):
     """
     Build a string with line breaks & indentation for dicts with more than one item.
 
@@ -31,12 +75,11 @@ def dict_with_conditional_line_breaks(dct, delimiter='=', line_break=' \\\n', in
     :param quote_escape: Optional flag to indicate if each key and value should be escaped with double quotes
     :return: Multi-line string that is well formed for human readers
     """
-    key_value_fmt = '"{}"' if quote_escape else '{}'
-    return (line_break + ' ' * indent).join(key_value_fmt.format(k) + delimiter + key_value_fmt.format(v)
-                                            for k, v in dct.items())
+    pairs = (delimiter.join((_escape(k, quote_escape), _escape(v, quote_escape))) for k, v in dct.items())
+    return _line_break_join(pairs, line_break, indent)
 
 
-def str_join_with_conditional_delimiter(parts, delimiter=None):
+def str_join_with_conditional_delimiter(parts, delimiter=DEFAULT_STR_JOIN_DELIMITER):
     """
     Build a string by performing an optional join if given a iterable and delimiter.
 
