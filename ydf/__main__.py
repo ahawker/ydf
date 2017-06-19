44b0b84be30400a8bea7b416489f2ff1f105354b
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-06-18 20:58:52 -0700

    Add __main__.py to make ydf package executable.

---
+++ ydf/__main__.py
@@ -0,0 +1,12 @@
+"""
+    ydf/__main__
+    ~~~~~~~~~~~~
+
+    Contains package level entrypoint for the :mod:`~ydf.cli` module.
+"""
+
+
+if __name__ == '__main__':
+    from ydf import cli
+
+    cli.main()
